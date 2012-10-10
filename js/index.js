b1125c14dc997e142c3931473b29197d1e9566c9
Ryan Abbott <rabbott4927@gmail.com>
2012-10-10 13:25:21 -0400

    Changed second page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   var height = $(window).height(); //finds the height of the window
   $(".container-fluid").css("margin-top", ((height / 2) - 200) + "px"); //centers the container verticaly
-  $(".page-two").css("margin-top", ((height/2)-150)+"px")
+  $(".page-two").css("top", ((height/2)+200)+"px")
+ // $("#learnmore").click(window.scrollto(0,height));
 });
