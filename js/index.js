aa54205531040f44fbe25d96ab076ae49b5f529c
Ryan Abbott <rabbott4927@gmail.com>
2012-10-10 13:05:29 -0400

    Added second page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
   var height = $(window).height(); //finds the height of the window
   $(".container-fluid").css("margin-top", ((height / 2) - 200) + "px"); //centers the container verticaly
+  $(".page-two").css("margin-top", ((height/2)-150)+"px")
 });
