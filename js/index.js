42116d7ddb16e3353687a4fc5f44a8f672e2323e
Ryan Abbott <rabbott4927@gmail.com>
2012-10-10 16:53:04 -0400

    Added scroll functionality to the learn more button, made the container re-center when the screen is resized

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
   var height = $(window).height(); //finds the height of the window
   $(".container-fluid").css("margin-top", ((height / 2) - 200) + "px"); //centers the container verticaly
-  $(".page-two").css("top", ((height/2)+200)+"px")
- // $("#learnmore").click(window.scrollto(0,height));
+  $(".page-two").css("top", ((height/2)+200)+"px");
+  $("#learnmore").click(function() {
+    window.scroll(0,-height);
+    window.scroll(0,height-40);
+  });
+  $(window).resize(function(){
+    var height = $(window).height(); //finds the height of the window
+    $(".container-fluid").css("margin-top", ((height / 2) - 200) + "px"); //centers the container verticaly
+    $(".page-two").css("top", ((height/2)+200)+"px");
+    $("#learnmore").click(function() {
+      window.scroll(0,-height);
+      window.scroll(0,height-40);
+    });
+  });
 });
