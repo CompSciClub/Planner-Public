5c2f287d22776920188cbe8327956b89378d520c
Ryan Abbott <rabbott4927@gmail.com>
2012-10-10 10:41:41 -0400

    Stopped text from wrapping, set image size(400x600), moved js into js/index.js

---
+++ js/index.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  var height = $(window).height(); //finds the height of the window
+  $(".container-fluid").css("margin-top", ((height / 2) - 200) + "px"); //centers the container verticaly
+});
