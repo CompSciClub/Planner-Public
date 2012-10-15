41b942d006cddb0d79a2e1b887fa36f1649822dd
Ryan Abbott <rabbott4927@gmail.com>
2012-10-14 22:11:23 -0400

    Fixed bugs caused by the previous commit

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
   var height = $(window).height(); //finds the height of the window
-  $(".container").css("margin-top", ((height / 2) - 200) + "px"); //centers the container verticaly
-  $(".page-two").css("top", ((height/2)+200)+"px");
+  $(".container.content").css("margin-top", ((height / 2) - 200) + "px"); //centers the container verticaly
+  $(".page-two").css("top", ((height/2)+350)+"px");
   $("#learnmore").click(function() {
     window.scroll(0,-height);
-    window.scroll(0,height-40);
+    window.scroll(0,height);
   });
   $(window).resize(function(){
     var height = $(window).height(); //finds the height of the window
-    $(".container").css("margin-top", ((height / 2) - 200) + "px"); //centers the container verticaly
-    $(".page-two").css("top", ((height/2)+200)+"px");
+    $(".container.content").css("margin-top", ((height / 2) - 200) + "px"); //centers the container verticaly
+    $(".page-two").css("top", ((height/2)+350)+"px");
     $("#learnmore").click(function() {
       window.scroll(0,-height);
-      window.scroll(0,height-40);
+      window.scroll(0,height);
     });
   });
 });
