a94e715cd2d0a41f67210baac5f2b628a452983b
Jason Teplitz <Jason@Jason-Teplitzs-Mac.local>
2012-10-16 23:08:05 -0400

    moved How it works to below screen

--- js/index.js
+++ js/index.js
@@ -1,18 +1,12 @@
 $(document).ready(function(){
-  var height = $(window).height(); //finds the height of the window
-  $(".container.content").css("margin-top", ((height / 2) - 200) + "px"); //centers the container verticaly
-  $(".page-two").css("top", ((height/2)+350)+"px");
-  $("#learnmore").click(function() {
-    window.scroll(0,-height);
-    window.scroll(0,height);
-  });
-  $(window).resize(function(){
-    var height = $(window).height(); //finds the height of the window
-    $(".container.content").css("margin-top", ((height / 2) - 200) + "px"); //centers the container verticaly
-    $(".page-two").css("top", ((height/2)+350)+"px");
-    $("#learnmore").click(function() {
-      window.scroll(0,-height);
-      window.scroll(0,height);
-    });
-  });
+  centerContainer();
+
+  $(window).resize(centerContainer);
 });
+
+function centerContainer(){
+  var height = $(window).height(); //finds the height of the window
+  console.log("height ", height);
+  $(".container.content").css("margin-top", ((height / 2) - 200) + "px"); //centers the container verticaly
+  $(".page-two").css("top", ((height))+"px");
+}
