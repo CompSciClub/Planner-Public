69458f200fe16c4423c2e89d16fb05701fa44f4c
Ryan Abbott <rabbott2937@gmail.com>
2013-02-18 13:11:59 -0500

    added a function to created additional assignments on the assignemnt sheet page

--- js/assignment_sheet.js
+++ js/assignment_sheet.js
@@ -1,2 +1,16 @@
+var current_assignment = 5;//saves how many assignments have been created
 $(document).ready(function(){
+  $("#more").click(function(){
+    add_assignment();
+  });
 });
+
+function add_assignment(){
+  $("#more").before('<div><div class="date span3" id="day'+String(current_assignment)+'"></div><input type="text" class="assignment span9"/></div>');
+//for readability:
+//  <div>
+//    <div class="date span3" id="day[current_assignment]"></div>
+//    <input type="text" class="assignment span9"/>
+//  <div>
+  current_assignment++;
+}
