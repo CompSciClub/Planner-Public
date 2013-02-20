ce182430939ca42f2945d03fcc328ae5d58f3239
Jason Teplitz <Jason@Jason-Teplitzs-MacBook-Pro.local>
2013-02-20 11:50:57 -0500

    added dates to the assignment sheet

--- js/assignment_sheet.js
+++ js/assignment_sheet.js
@@ -1,16 +1,46 @@
 var current_assignment = 5;//saves how many assignments have been created
+
+var lastDate;
 $(document).ready(function(){
+  writeDates();
   $("#more").click(function(){
     add_assignment();
   });
+
+  $("#Block").change(writeDates);
 });
 
 function add_assignment(){
-  $("#more").before('<div><div class="date span3" id="day'+String(current_assignment)+'"></div><input type="text" class="assignment span9"/></div>');
+  // get the date for this assignment
+  lastDate = findNextDate($("#Block").val());
+  var dateString = (lastDate.getMonth() + 1) + "/" + (lastDate.getDate()) + "/" + String(lastDate.getFullYear()).slice(-2);
+  $("#more").before('<div><div class="date span3" id="day'+String(current_assignment)+'">' + dateString + '</div><input type="text" class="assignment span9"/></div>');
 //for readability:
 //  <div>
 //    <div class="date span3" id="day[current_assignment]"></div>
 //    <input type="text" class="assignment span9"/>
 //  <div>
   current_assignment++;
+}
+
+// writes the dates for the existing date block
+function writeDates(){
+  lastDate = new Date(new Date() - 86400000); // reset lastDate to yesterday (so we can start with today) 
+
+  var dates = $(".date");
+  for (var i = 0; i < dates.length; i++){
+    lastDate = findNextDate($("#Block").val());
+    var dateString = (lastDate.getMonth() + 1) + "/" + (lastDate.getDate()) + "/" + String(lastDate.getFullYear()).slice(-2);
+    $(dates[i]).text(dateString);
+  }
+}
+
+// returns the next date that the given block has a class
+function findNextDate(block){
+  checkDate = new Date(lastDate.getTime() + 86400000);
+  while (!scheduleUtils.checkThisDate(block, checkDate)){
+    checkDate = new Date(checkDate.getTime() + 86400000) // check one day in the future
+  }
+
+  return checkDate;
 }
