3318c7096d2a419782133b16c30d406b9735afd9
Andy Sedlack <asedlack4@gmail.com>
2013-02-20 12:27:52 -0500

    Calls a function when the block is changed.

--- js/assignment_sheet.js
+++ js/assignment_sheet.js
@@ -3,11 +3,19 @@ $(document).ready(function(){
   $("#more").click(function(){
     add_assignment();
   });
+  $("#block").change(function(){
+    console.log("Ryan is annoying");
+    update_dates();
+  });
 });
+function update_dates(){
+  console.log("2+2 is 342342342342.");
+  }
+
 
 function add_assignment(){
   $("#more").before('<div><div class="date span3" id="day'+String(current_assignment)+'"></div><input type="text" class="assignment span9"/></div>');
-//for readability:
+//for readability: WE DON'T NEED YOUR GODD**N READABILITY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //  <div>
 //    <div class="date span3" id="day[current_assignment]"></div>
 //    <input type="text" class="assignment span9"/>
