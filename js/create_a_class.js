1b2fe1db8f20ffa829a49e0daec355bf13a4396b
Andy Sedlack <asedlack4@gmail.com>
2013-02-21 20:54:37 -0500

    Added some stuff the the create a class page.
    Also some js that I can't figure out how to make work... But should do
    something nifty later.

---
+++ js/create_a_class.js
@@ -0,0 +1,32 @@
+  /* This whole section here is rather non-functional, I'm not sure how to get the selected option from the HTML. Also, all these ifs are probably horribly inefficient. $("#blockSelect").change(function (var block) {  
+    block = document.getElementById("blockSelect").value;
+    if(option == "A"){ //This big thing should just check the block and then send it over to whatever function we'll have doin' stuff.
+      block = "A";
+    };
+    else if(option == "B") {
+      block = "B";
+    };
+    else if(option == "C") {
+      block = "C";
+    };
+    else if(option == "D") {
+      block = "D";
+    };
+    else if(option == "E") {
+      block = "E";
+    };
+    else if(option == "F") {
+      block = "F";
+    };
+    else if(option == "G") {
+      block = "G";
+    };
+    else {
+      block = "H";
+    };
+    console.log(block); //only here for debuggery in case this doesn't work...
+    setBlock (block);
+  });
+  
+  function setBlock(block);
+  */
